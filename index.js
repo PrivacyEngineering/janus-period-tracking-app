9cefd91b197722d7f31cace0f17254232dd74902
Paul Heinrich <paul.heinrich@rwth-aachen.de>
2021-06-22 10:41:28 +0200

    Add description for auth-directives usage.

--- index.js
+++ index.js
@@ -136,6 +136,11 @@ const server = new ApolloServer({
 /**
  * Run Server and pass sample request: 
  * query{libraries{branch,zip, books{title, author{name}}}}
+ * 
+ * Using auth-directives: Provide JWT in Header of request: 
+ * {
+  "Authorization": "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJHUkFORHN0YWNrIiwiaWF0IjoxNTQ5MTQ1Mjk0LCJleHAiOjE2OTE3ODEzMDcsImF1ZCI6ImdyYW5kc3RhY2suaW8iLCJzdWIiOiJib2JAbG9ibGF3LmNvbSIsIlJvbGUiOiJBRE1JTiIsIlNjb3BlIjpbIlVzZXI6UmVhZCIsIlVzZXI6Q3JlYXRlIiwiVXNlcjpVcGRhdGUiLCJVc2VyOkRlbGV0ZSJdfQ.WJffOec05r8KuzW76asax1iCzv5q4rwRv9kvFyw7c_E"
+}
  */
 server.listen().then(({ url }) => {
   console.log(`🚀  Server ready at ${url}`);
