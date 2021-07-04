7db340d1e0f55747f5ac612ec830aae27ed0c1fa
dextroseSH <pheinrich11235@gmail.com>
2021-07-04 21:14:43 +0200

    add noise for pain

--- server/src/index.js
+++ server/src/index.js
@@ -11,7 +11,7 @@ const dotenv = require("dotenv");
 
 dotenv.config();
 
-NoiseDirective.prototype.getArgumentForRole = function(){
+NoiseDirective.prototype.getArgumentForRoles = function(){
   //add here { role -> Arguments }
 }
 
