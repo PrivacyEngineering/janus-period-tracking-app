2d65eb0fb4b06a23fc31a7411005feb09ff20408
kipke161 <kipke@campus.tu-berlin.de>
2021-07-04 03:04:54 +0200

    Added Schema for DB Models

--- server/src/schema.js
+++ server/src/schema.js
@@ -1,16 +1,12 @@
 const { gql } = require('apollo-server')
 
-
-//scalar Date 
-
-
 const typeDefs = gql`
 directive @zipsupp on FIELD_DEFINITION
 directive @isAuthenticated on OBJECT | FIELD_DEFINITION
 directive @hasRole(roles: [Role]) on OBJECT | FIELD_DEFINITION
 directive @hasScope(scopes: [String]) on OBJECT | FIELD_DEFINITION
 directive @addNoise on FIELD_DEFINITION
-
+scalar Dates
 enum Role {
   reader
   user
@@ -20,12 +16,13 @@ enum Role {
 
 # A User 
   type User {
-    id: ID! @addNoise
+    id: ID! 
     username: String!
     firstName: String
+    lastName: String
+    email: String
     passwordHash: String
-    role: String!
-    email: String°
+    role: String
     age: Int 
     contraceptive: String
   # A user can have more than one cycle 
@@ -42,8 +39,8 @@ enum Role {
   # A cycle has an id, start and end Date and required to have a user
   type Cycle {
     id: ID!
-    start: Date
-    end: Date
+    start: Dates
+    end: Dates
   # It's required that every cycle has a user
   # Hence the exclamation (!) to mark it as required
     hasUser: User!
@@ -52,7 +49,7 @@ enum Role {
   # A symptom has an id, date of that the symptom appeared and required to have a user
   type Symptom{
     id: ID!
-    date: Date
+    date: Dates
     symptom: String
   # It's required that every Symptom has a user
   # Hence the exclamation (!) to mark it as required
@@ -62,17 +59,22 @@ enum Role {
 
   # Query type defines the available queries for clients to execute
   type Query {
-    users: [User] @hasScope(scopes:["User:Read"])
-    cycle: [Cycle]
-    allSymptoms: [Symptom]
-    user(id: Int!): User
+    #user: [User] @hasScope(scopes:["User:Read"])
+    getUser(id: Int!): User!
+    allUsers: [User!]!
+    getCycle(id: Int!): Cycle!
+    #[User!] means that it's okay for a user not to have a Cycle, but if she/he does have, it must be of type Cycle
+    allCycles: [Cycle!]!
+    getSymptom(id: Int!): Symptom!
+    allSymptoms: [Symptom!]!
   }
-  
 
-  type Mutation {
+
+  #type Mutation {
     # TODO add rest of the fields
-    createUser(username: String!, email: String!, passwordHash: String!): User!
-  }
-`;
+  #  createUser(username: String!, email: String!, passwordHash: String!): User!
+#  }
+`
+;
 
 module.exports = typeDefs;
