946732d39c583a4121dc8aa1ccff352311b2c218
dextroseSH <pheinrich11235@gmail.com>
2021-07-22 15:00:53 +0200

    Added azure postgres comment

--- server/models/index.js
+++ server/models/index.js
@@ -3,6 +3,7 @@
 const Sequelize = require('sequelize');
 
 const sequelize = new Sequelize('postgres://user:password@localhost:5432/period-tracking');
+//const sequelize = new Sequelize('postgres://myadmin@dbpeng2:VjYK5YcGMswGU7FFd9hfy6kpYm8267XquSMSusaV@dbpeng2.postgres.database.azure.com:5432/postgres');
 
 const models = {
   User: require("./user")(sequelize, Sequelize.DataTypes),
